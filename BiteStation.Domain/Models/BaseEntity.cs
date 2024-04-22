22c9ffba179eee04ce674021a859ef27cc54516e
Obay Ismaeel <obayhany@gmail.com>
2024-04-23 00:40:04 +0300

    Changed the default time to be UTC

--- BiteStation.Domain/Models/BaseEntity.cs
+++ BiteStation.Domain/Models/BaseEntity.cs
@@ -8,6 +8,6 @@ namespace BiteStation.Domain.Models;
 public abstract class BaseEntity
 {
     public int Id { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public DateTime UpdatedAt { get; set;} = DateTime.Now;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set;} = DateTime.UtcNow;
 }
