ad7bcda122d8d4b01faa24999701c5cb46c55e58
Obay Ismaeel <obayhany@gmail.com>
2024-04-22 23:58:54 +0300

    Created unit of work and base repository interfaces

---
+++ BiteStation.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using BiteStation.Domain.Models;
+
+namespace BiteStation.Domain.Abstractions;
+public interface IUnitOfWork
+{
+    IBaseRepository<User> Users { get; }
+    IBaseRepository<Restaurant> Restaurants { get; }
+    IBaseRepository<Menu> Menus { get; }
+    IBaseRepository<Item> Items { get; }
+    IBaseRepository<Order> Orders { get; }
+    IBaseRepository<OrderItem> OrderItems { get; }
+    Task<int> CompleteAsync();
+}
