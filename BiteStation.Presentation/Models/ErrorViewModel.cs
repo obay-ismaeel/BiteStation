12b55e36870d9f6a85827d63baf77667c5391451
Obay Ismaeel <obayhany@gmail.com>
2024-04-23 03:30:23 +0300

    Removed not needed folders/files

--- BiteStation.Presentation/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BiteStation.Presentation.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
