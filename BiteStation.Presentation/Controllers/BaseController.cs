b7bc62e6441adb43f42397153b78658a65cbd300
Obay Ismaeel <obayhany@gmail.com>
2024-04-23 03:29:46 +0300

    Created a base controller that all controllers will inherit from

---
+++ BiteStation.Presentation/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using BiteStation.Domain.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiteStation.Presentation.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BaseController : ControllerBase
+{
+    public readonly IUnitOfWork _unitOfWork;
+    public BaseController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+}
