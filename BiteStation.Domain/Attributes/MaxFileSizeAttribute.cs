b913b6ae7154523d84092f59a775a78f4dc84bfa
Obay Ismaeel <obayhany@gmail.com>
2024-04-24 23:45:55 +0300

    Created image file validation attributes

---
+++ BiteStation.Domain/Attributes/MaxFileSizeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace BiteStation.Domain.Attributes;
+public class MaxFileSizeAttribute : ValidationAttribute
+{
+    private readonly int _maxFileSize;
+    public MaxFileSizeAttribute(int maxFileSize)
+    {
+        _maxFileSize = maxFileSize;
+    }
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var file = value as IFormFile;
+
+        if (file is not null)
+        {
+            if (file.Length > _maxFileSize)
+            {
+                return new ValidationResult($"File size can't exceed {_maxFileSize} bytes");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
+
