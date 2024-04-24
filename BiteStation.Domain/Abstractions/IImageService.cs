786f6e0d5862ded4268150e9f985b09717e6744d
Obay Ismaeel <obayhany@gmail.com>
2024-04-24 23:44:02 +0300

    Created image store service interface and implementation

---
+++ BiteStation.Domain/Abstractions/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiteStation.Domain.Abstractions;
+public interface IImageService
+{
+    Task<string> StoreAsync(IFormFile image);
+    void Delete(string filePath);
+}
---
+++ BiteStation.Infrastructure/ImageService.cs
@@ -0,0 +1,34 @@
+﻿using BiteStation.Domain.Abstractions;
+using BiteStation.Domain.Constants;
+using Microsoft.AspNetCore.Http;
+
+namespace BiteStation.Infrastructure;
+public class ImageService : IImageService
+{
+    public async Task<string> StoreAsync(IFormFile image)
+    {
+        if (image == null || image.Length == 0)
+        {
+            return null;
+        }
+
+        var uniqueFileName = Guid.NewGuid().ToString() + "_" + image.FileName;
+
+        var filePath = Path.Combine(FileSettings.ImagesPath, uniqueFileName);
+
+        var fullPath = Path.Combine(FileSettings.WebRootPath, filePath);
+
+        using (var stream = new FileStream(fullPath, FileMode.Create))
+        {
+            await image.CopyToAsync(stream);
+        }
+
+        return filePath;
+    }
+
+    public void Delete(string filePath)
+    {
+        var fullPath = Path.Combine(FileSettings.WebRootPath, filePath);
+        File.Delete(fullPath);
+    }
+}
