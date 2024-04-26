ec5d8ec1f6d3a194fcfe790d114eeef71c5d68bf
Obay Ismaeel <obayhany@gmail.com>
2024-04-26 13:58:25 +0300

    Added services to the base controller

--- BiteStation.Presentation/Controllers/BaseController.cs
+++ BiteStation.Presentation/Controllers/BaseController.cs
@@ -1,15 +1,23 @@
-﻿using BiteStation.Domain.Abstractions;
+﻿using AutoMapper;
+using BiteStation.Domain.Abstractions;
+using BiteStation.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace BiteStation.Presentation.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[EnableRateLimiting("fixed")]
 public class BaseController : ControllerBase
 {
     public readonly IUnitOfWork _unitOfWork;
-    public BaseController(IUnitOfWork unitOfWork)
+    public readonly IMapper _mapper;
+    public readonly IImageService _imageService;
+    public BaseController(IUnitOfWork unitOfWork, IMapper mapper, IImageService imageService)
     {
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
+        _imageService = imageService;
     }
 }
