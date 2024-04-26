f54ad2c896fed28f3bb0c3c311af8a5199cc16c1
Obay Ismaeel <obayhany@gmail.com>
2024-04-26 13:55:59 +0300

    Created automapper mapping profile class

---
+++ BiteStation.Presentation/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BiteStation.Domain.Models;
+using BiteStation.Presentation.Dtos;
+
+namespace BiteStation.Presentation.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Restaurant, OutgoingRestaurantDto>();
+        CreateMap<IncomingRestaurantDto, Restaurant>();
+    }
+}
