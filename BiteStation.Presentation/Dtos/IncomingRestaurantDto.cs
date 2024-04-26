10e1077fa1942740a1b61f9c4f3351b704e82d95
Obay Ismaeel <obayhany@gmail.com>
2024-04-26 11:36:49 +0300

    Created restaurant DTOs

---
+++ BiteStation.Presentation/Dtos/IncomingRestaurantDto.cs
@@ -0,0 +1,15 @@
+﻿using BiteStation.Domain.Attributes;
+using BiteStation.Domain.Constants;
+
+namespace BiteStation.Presentation.Dtos;
+
+public class IncomingRestaurantDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    [MaxFileSize(FileSettings.MaxFileSizeInBytes)]
+    [AllowedExtensions(FileSettings.AllowedExtensions)]
+    public IFormFile ImageFile { get; set; }
+}
---
+++ BiteStation.Presentation/Dtos/OutgoingRestaurantDto.cs
@@ -0,0 +1,13 @@
+﻿using BiteStation.Domain.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace BiteStation.Presentation.Dtos;
+
+public class OutgoingRestaurantDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public string ImagePath { get; set; }
+}
