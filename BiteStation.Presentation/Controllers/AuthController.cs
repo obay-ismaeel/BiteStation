84fc0a0aa3d0382263bdbbd7f1f38d5105634ccf
Obay Ismaeel <obayhany@gmail.com>
2024-05-06 23:50:11 +0300

    Created auth controller

---
+++ BiteStation.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using BiteStation.Domain.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiteStation.Presentation.Controllers;
+
+public class AuthController : BaseController
+{
+    public AuthController(IUnitOfWork unitOfWork, IMapper mapper, IImageService imageService) : base(unitOfWork, mapper, imageService)
+    {
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("forget-password")]
+    public async Task<IActionResult> ForgetPassword()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("confirm-email")]
+    public async Task<IActionResult> ConfirmEmail()
+    {
+        throw new NotImplementedException();
+    }
+}
